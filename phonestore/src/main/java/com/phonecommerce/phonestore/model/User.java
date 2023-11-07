279fa26956be2d74cd247a8e5c2e039d567c1686
KonstantinDichev <kdichev.nl@gmail.com>
2023-11-07 23:52:02 +0100

    Add User model

---
+++ phonestore/src/main/java/com/phonecommerce/phonestore/model/User.java
@@ -0,0 +1,35 @@
+package com.phonecommerce.phonestore.model;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public User(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
