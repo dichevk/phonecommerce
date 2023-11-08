40b2337c4fb54ef1a1d61f02b62c24a8ca7b2424
KonstantinDichev <kdichev.nl@gmail.com>
2023-11-08 18:37:02 +0100

    Add Review model

---
+++ phonestore/src/main/java/com/phonecommerce/phonestore/model/Review.java
@@ -0,0 +1,25 @@
+package com.phonecommerce.phonestore.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String comment;
+    private int rating;
+
+    @ManyToOne
+    private User user; // The user who wrote the review
+
+    @ManyToOne
+    private Phone phone; // The phone being reviewed
+}
